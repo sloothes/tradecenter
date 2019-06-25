97d920436d637c0d9ef3ca2f0be3256f68ba1e41
Jane Slooth <sloothes.com@gmail.com>
2019-06-25 23:14:09 +0300

    Add files via upload

---
+++ geometries/elevator-doorleaf1.js
@@ -0,0 +1,34 @@
+{
+    "normals":[0,0,1,-0,0,-1,-0,1,-0,-1,0,0,0,-1,0,1,-0,-0],
+    "uvs":[[0.371,0.058,0.117,0.058,0.371,0.94,0.117,0.94]],
+    "metadata":{
+        "normals":6,
+        "uvs":1,
+        "vertices":8,
+        "generator":"io_three",
+        "type":"Geometry",
+        "version":3,
+        "faces":6,
+        "materials":1
+    },
+    "vertices":[0.189,0.186,0.225,-0.516,0.186,0.225,-0.516,0.186,-9.976,0.189,0.186,-9.976,0.189,35.669,0.225,-0.516,35.669,0.225,-0.516,35.669,-9.976,0.189,35.669,-9.976],
+    "faces":[43,0,1,2,3,0,0,0,1,1,0,0,0,0,43,4,7,6,5,0,2,3,3,2,1,1,1,1,43,0,4,5,1,0,0,2,2,0,2,2,2,2,43,1,5,6,2,0,0,2,3,1,3,3,3,3,43,2,6,7,3,0,1,3,3,1,4,4,4,4,43,4,0,3,7,0,2,0,1,3,5,5,5,5],
+    "name":"elevator_leaf1",
+    "materials":[{
+        "specularCoef":51,
+        "visible":true,
+        "blending":"NormalBlending",
+        "colorEmissive":[0,0,0],
+        "DbgName":"metal",
+        "colorDiffuse":[0.474,0.474,0.474],
+        "DbgIndex":0,
+        "depthTest":true,
+        "transparent":true,
+        "opacity":1,
+        "shading":"standard",
+        "DbgColor":15658734,
+        "colorSpecular":[0.072,0.072,0.072],
+        "wireframe":false,
+        "depthWrite":true
+    }]
+}
---
+++ geometries/elevator-doorleaf2.js
@@ -0,0 +1,34 @@
+{
+    "normals":[0,0,1,-0,0,-1,-0,1,-0,-1,0,0,0,-1,0,1,-0,-0],
+    "uvs":[[0.326,0.058,0.073,0.058,0.326,0.939,0.073,0.939]],
+    "metadata":{
+        "normals":6,
+        "uvs":1,
+        "vertices":8,
+        "generator":"io_three",
+        "type":"Geometry",
+        "version":3,
+        "faces":6,
+        "materials":1
+    },
+    "vertices":[0.934,0.186,9.902,0.23,0.186,9.902,0.23,0.186,-0.3,0.934,0.186,-0.3,0.934,35.669,9.902,0.23,35.669,9.902,0.23,35.669,-0.3,0.934,35.669,-0.3],
+    "faces":[43,0,1,2,3,0,0,0,1,1,0,0,0,0,43,4,7,6,5,0,2,3,3,2,1,1,1,1,43,0,4,5,1,0,0,2,2,0,2,2,2,2,43,1,5,6,2,0,0,2,3,1,3,3,3,3,43,2,6,7,3,0,1,3,3,1,4,4,4,4,43,4,0,3,7,0,2,0,1,3,5,5,5,5],
+    "name":"elevator_leaf2",
+    "materials":[{
+        "specularCoef":51,
+        "visible":true,
+        "blending":"NormalBlending",
+        "colorEmissive":[0,0,0],
+        "DbgName":"metal",
+        "colorDiffuse":[0.474,0.474,0.474],
+        "DbgIndex":0,
+        "depthTest":true,
+        "transparent":true,
+        "opacity":1,
+        "shading":"standard",
+        "DbgColor":15658734,
+        "colorSpecular":[0.072,0.072,0.072],
+        "wireframe":false,
+        "depthWrite":true
+    }]
+}
